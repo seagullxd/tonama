c0463ec39b979542b55e485e21051d8d2990cebc
seagullxd <chenly219@hotmail.com>
2025-11-08 21:01:30 +1000

    Add dialog handler

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,16 @@
 import { countryColours } from './colours.js';
 import PlayerCards from './player_cards.js';
+import { 
+  toTitleCase, 
+  doesNotContainOnlyLettersSpaces, 
+  doesNotContainMisplacedCapital 
+} from './helper.js';
+import { 
+  duplicateGuessErrorMessage, 
+  invalidCountryErrorMessage, 
+  duplicateGuessTag, 
+  invalidCountryTag 
+} from './errors.js'
 
 // https://developer.mozilla.org/en-US/docs/Web/SVG/Guides/Scripting
 function getSvg() {
@@ -59,7 +70,7 @@ function addTempParagraph(message, messageType) {
     const pElement = document.createElement("p")
     pElement.setAttribute("class", "message")
     pElement.setAttribute("id", messageType)
-    
+
     const sampElement = document.createElement("samp")
     sampElement.textContent = message; 
     node.appendChild(pElement);
@@ -72,23 +83,82 @@ function addTempParagraph(message, messageType) {
   }
 }
 
-function displayMessage(country, distance, colour) {
-  const textCoordinateX = "30"; 
-  const textCoordinateY = "180"; 
-  const articleContainer = "core"
+function isGuessTooLong(guess) {
+  const maxGuessLength = 56 // based on the longest country name 
+  return guess.length > maxGuessLength  
+}
 
-  let svg = getSvg();
-  let node = document.getElementById(guessesContainer);
-  node.appendChild(svg)
-  let r = getRect(colour);
-  svg.appendChild(r);
-  let t1 = getText(textCoordinateX, country);
-  svg.appendChild(t1);
-  let t2 = getText(textCoordinateY, distance);
-  svg.appendChild(t2);
+function isGuessInvalid(guess) {
+  // todo: handle Uppercase letters that occur after the first letter e.g., eNglanD
+  console.log(isGuessTooLong(guess))
+  console.log(doesNotContainOnlyLettersSpaces(guess)) // good = 
+  console.log(!(guess in countryColours)) // true 
+  console.log(doesNotContainMisplacedCapital(guess)) // true 
+  return isGuessTooLong(guess) || doesNotContainOnlyLettersSpaces(guess) 
+    || !(toTitleCase(guess) in countryColours) || doesNotContainMisplacedCapital(guess)
+}
+
+function handleDialog() {
+  const moreBtn = document.getElementById("more-btn");
+  const restartBtn = document.getElementById("restart-btn");
+  const levelBtn = document.getElementById("level-btn");
+  const howToPlayBtn = document.getElementById("how-to-play-btn");
+  const faqBtn = document.getElementById("faq-btn");
+  const settingsBtn = document.getElementById("settings-btn");
+
+  const moreDialog = document.getElementById("moreDialog");
+  const restartDialog = document.getElementById("restartDialog");
+  const levelDialog = document.getElementById("levelDialog");
+  const howToPlayDialog = document.getElementById("howToPlayDialog");
+  const faqDialog = document.getElementById("faqDialog");
+  const settingsDialog = document.getElementById("settingsDialog");
+
+  const closeBtns = document.querySelectorAll(".close");
+
+  moreBtn.addEventListener("click", () => {
+    moreDialog.showModal();
+  });
+
+  restartBtn.addEventListener("click", () => {
+    handleDialogClose(closeBtns)
+    restartDialog.showModal();
+  });
+
+  levelBtn.addEventListener("click", () => {
+    handleDialogClose(closeBtns)
+    levelDialog.showModal();
+  });
+
+  howToPlayBtn.addEventListener("click", () => {
+    handleDialogClose(closeBtns)
+    howToPlayDialog.showModal();
+  });
+
+  faqBtn.addEventListener("click", () => {
+    handleDialogClose(closeBtns)
+    faqDialog.showModal();
+  });
+
+  settingsBtn.addEventListener("click", () => {
+    handleDialogClose(closeBtns)
+    settingsDialog.showModal();
+  });
+
+  closeBtns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.parentElement.close();
+    });
+  });
+}
+
+function handleDialogClose(closeBtns) {
+  closeBtns.forEach((btn) => {
+      btn.parentElement.close();
+  });
 }
 
 function main() {
+  handleDialog()
 
   const formElem = document.querySelector("form");
   formElem.addEventListener("submit", (e) => {
@@ -99,8 +169,7 @@ function main() {
     new FormData(formElem);
   });
 
-  const playerCards = new PlayerCards();
-  let hasAlreadyBeenGuessed = false 
+
   formElem.addEventListener("formdata", (e) => {
     console.log("formdata fired");
 
@@ -111,11 +180,17 @@ function main() {
       guess = value 
     }
 
-    if (!playerCards.hasCard(guess)) {
-      playerCards.addCard(guess, countryColours[guess])
-      displayCard(guess, "500km", countryColours[guess])
+    const playerCards = new PlayerCards();
+    if (!isGuessInvalid(guess)) {
+      const titleCasedGuess = toTitleCase(guess);
+      if (!playerCards.hasCard(titleCasedGuess)) {
+        playerCards.addCard(titleCasedGuess, countryColours[titleCasedGuess])
+        displayCard(titleCasedGuess, "500km", countryColours[titleCasedGuess])
+      } else {
+        addTempParagraph(`${titleCasedGuess} ${duplicateGuessErrorMessage}`, duplicateGuessTag)
+      }      
     } else {
-      addTempParagraph(`${guess} has already been guessed.`, "guessed-message")
+      addTempParagraph(`${guess} ${invalidCountryErrorMessage}`, invalidCountryTag)
     }
   });
 }
